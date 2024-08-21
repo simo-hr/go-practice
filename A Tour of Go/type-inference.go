5342bca22c40603b78cce1fe61100e7594f630d9
github xxbbxx825 <xxbbxx825@gmail.com>
2024-08-21 21:35:33 +0900

    A Tour of Go: Complete Basics-1

---
+++ A Tour of Go/type-inference.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// v := 42 // change me!
+	// v := 3.14
+	// v := 3i
+	// v := 'a'
+	// v := 'a' + 3
+	v := "a" + 3
+	fmt.Printf("v is of type %T\n", v)
+}
